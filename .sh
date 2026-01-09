5e133071fd664ed84983410b876d94fa264ff6eb
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-01-08 23:00:09 -0800

    -

--- .sh
+++ .sh
@@ -22,45 +22,48 @@ exec &> logs/main.log
 		! -name config.yml \
 		| sort
 	)
-} || echo "'$yml' not found" >&2
+} || {
+	echo "'$yml' not found" >&2
+	exit 1
+}
+declare -A toggles
+toggles[details]=true
+toggles[plot]=true
 [[ -f $html ]] && {
-	$html h1 Characters
-	while IFS= read -r c; do
-		$html h2 "$(jq -r '.name' <<< "$c")"
-		$html ul "$(
-			m=`jq -r '.monikers | length != 0' <<< "$c"`
-			r=`jq -r '.relationships | length != 0' <<< "$c"`
-			if [[ $m = true ]] || [[ $r = true ]]; then
-				if [[ $m = true ]]; then
-					$html li "$(
-						$html b Monikers:
-						$html ul "$(
-							while IFS= read -r i; do
-								$html li "$i"
-							done < <(jq -r '.monikers[]' <<< "$c")
-						)"
-					)"
+	$html style 'body{font:20pt"Noto Sans"}'
+	"${toggles[details]}" && $html div "$(
+		while IFS= read -r c; do
+			$html h1 "$(jq -r '.name' <<< "$c")"
+			$html dl "$(
+				m=`jq -r '.monikers | length != 0' <<< "$c"`
+				r=`jq -r '.relationships | length != 0' <<< "$c"`
+				if [[ $m = true ]] || [[ $r = true ]]; then
+					if [[ $m = true ]]; then
+						$html dt "$($html b Monikers:)"
+						while IFS= read -r i; do
+							$html dd "$($html q "$i")"
+						done < <(jq -r '.monikers[]' <<< "$c")
+					fi
+					if [[ $r = true ]]; then
+						$html dt "$($html b Relationships:)"
+						while IFS= read -r i; do
+							$html dd "$i"
+						done < <(jq -r '.relationships[]' <<< "$c")
+					fi
 				fi
-				if [[ $r = true ]]; then
-					$html li "$(
-						$html b Relationships:
-						$html ul "$(
-							while IFS= read -r i; do
-								$html li "$i"
-							done < <(jq -r '.relationships[]' <<< "$c")
-						)"
-					)"
-				fi
-			fi
-		)"
-	done < <(jq -c '.[]' data/characters.json)
-	$html h1 Story
-	while IFS= read -r l; do
-		k=`jq -r '.key' <<< "$l"`
-		v=`jq -r '.value' <<< "$l"`
-		$html p "$(
-			$html b "$k:"
-			printf " $v"
-		)"
-	done < <(jq -c '.[] | to_entries[]' data/script.json)
-} > README.md || echo "'$html' not found" >&2
+			)"
+		done < <(jq -c '.[]' data/characters.json)
+	)"
+	"${toggles[details]}" && "${toggles[plot]}" && $html hr
+	"${toggles[plot]}" && $html dl "$(
+		while IFS= read -r l; do
+			$html dt "$($html b "$(jq -r '.key' <<< "$l"):")"
+			$html dd "$(jq -r '.value' <<< "$l")"
+		done < <(jq -c '.[] | to_entries[]' data/script.json)
+	)"
+} > README.md || {
+	echo "'$html' not found" >&2
+	exit 1
+}
+cp README.md index.html
+find . -empty -delete
